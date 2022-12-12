2070c6c3567df983cf9a95d68be0113b4eec28ca
S4nxez <119665698+S4nxez@users.noreply.github.com>
2022-12-12 19:52:11 +0100

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hola2");
         System.out.println("hola3");
+        System.out.println("Hola desde github");
     }
 }
